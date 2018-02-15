2bfd249766cf69df0527e1fd5e9fcf9c6823084c
Michael Weber <webermn15@gmail.com>
2018-02-15 16:22:54 -0600

    set up all accessible views from the landing page, added a ton of styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Login from './Login'
 import Register from './Register'
 import Main from './Main'
-import CreateHunt from './CreateHunt'
 
 class App extends Component {
   constructor(){
